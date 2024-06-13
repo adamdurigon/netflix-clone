948c88fc8920e9df93dbf9a6be6b218e497ba16d
Adam <durigonadam@gmail.com>
2024-06-13 20:29:49 +0200

    final

--- pages/profiles.tsx
+++ pages/profiles.tsx
@@ -1,36 +1,35 @@
-import useCurrentUser from "@/hooks/useCurrentUser"
-import { NextPageContext } from "next"
-import { getSession } from "next-auth/react"
-import { useRouter } from "next/router"
+import useCurrentUser from "@/hooks/useCurrentUser";
+import { NextPageContext } from "next";
+import { getSession } from "next-auth/react";
+import { useRouter } from "next/router";
 
 export async function getServerSideProps(context: NextPageContext) {
-    const session = await getSession(context)
+    const session = await getSession(context);
 
-    if(!session) {
+    if (!session) {
         return {
             redirect: {
                 destination: '/auth',
                 permanent: false,
-            }
-        }
+            },
+        };
     }
 
     return {
-        props: {}
-    }
+        props: {},
+    };
 }
 
 const Profiles = () => {
-    const router = useRouter()
-    const { data: user } = useCurrentUser()
+    const router = useRouter();
+    const { data: user } = useCurrentUser();
 
     return (
         <div className="flex items-center h-full justify-center">
             <div className="flex flex-col">
-                <h1 className="text-3xl md:text-6xl text-white text-center" >Who is watching?</h1>
+                <h1 className="text-3xl md:text-6xl text-white text-center">Who is watching?</h1>
                 <div className="flex items-center justify-center gap-8 mt-10">
                     <div onClick={() => router.push('/')}>
-
                         <div className="group flex-row w-44 mx-auto">
                             <div
                                 className="
@@ -66,7 +65,7 @@ const Profiles = () => {
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Profiles
+export default Profiles;
