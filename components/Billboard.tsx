86ba65e10e07b7bd19c29f0acf273fb486cd873e
Adam <durigonadam@gmail.com>
2024-06-27 20:38:22 +0200

    final

--- components/Billboard.tsx
+++ components/Billboard.tsx
@@ -1,14 +1,94 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { AiOutlineInfoCircle } from "react-icons/ai";
 import PlayButton from "./PlayButton";
+import useBillboard from "@/hooks/useBillboard";
+import useInfoModal from "@/hooks/useInfoModal";
 
 interface BillboardProps {
   movie: Record<string, any> | null;
 }
 
 const Billboard: React.FC<BillboardProps> = ({ movie }) => {
-  if (!movie) return null;
+  const { data } = useBillboard();
+  const { openModal } = useInfoModal();
 
+  const handleOpenModal = useCallback(() => {
+    openModal(data?.id);
+  }, [openModal, data?.id]);
+
+  if (!movie) {
+    // Utiliser les données de data si movie n'est pas défini
+    return (
+      <div className="relative h-[56.25vw]">
+        <video
+          className="
+            w-full
+            h-[56.25vw]
+            object-cover
+            brightness-[60%]
+          "
+          autoPlay
+          muted
+          loop
+          poster={data?.thumbnailUrl}
+          src={data?.videoUrl}
+        ></video>
+        <div className="absolute top-[30%] md:top-[40%] ml-4 md:ml-16">
+          <p className="
+            text-white
+            text-1xl 
+            md:text-5xl 
+            h-full w-[50%] 
+            lg:text-6xl 
+            font-bold 
+            drop-shadow-xl
+          ">
+            {data?.title}
+          </p>
+          <p className="
+            text-white
+            text-[8px]
+            md:text-lg
+            mt-3
+            md:mt-8
+            w-[90%]
+            md:w-[80%]
+            lg:w-[50%]
+            drop-shadow-xl
+          ">
+            {data?.description}
+          </p>
+          <div className="flex felx-row items-center mt-3 md:mt-4 gap-3">
+            <PlayButton movieId={data?.id} />
+            <button
+              onClick={handleOpenModal}
+              className="
+                bg-white
+                text-white
+                bg-opacity-30
+                rounded-md
+                py-1 md:py-2
+                px-1 md:px-4
+                w-auto
+                text-xs lg:text-lg
+                font-semibold
+                flex
+                flex-row
+                items-center
+                hover:bg-opacity-20
+                transition
+              "
+            >
+              <AiOutlineInfoCircle className="mr-1" />
+              More Info
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  // Sinon, utiliser les détails du film passé en props
   return (
     <div className="relative h-[56.25vw]">
       <video
@@ -22,8 +102,8 @@ const Billboard: React.FC<BillboardProps> = ({ movie }) => {
         muted
         loop
         poster={movie.thumbnailUrl}
-        src={movie.videoUrl}>
-      </video>
+        src={movie.videoUrl}
+      ></video>
       <div className="absolute top-[30%] md:top-[40%] ml-4 md:ml-16">
         <p className="
           text-white
@@ -52,6 +132,7 @@ const Billboard: React.FC<BillboardProps> = ({ movie }) => {
         <div className="flex felx-row items-center mt-3 md:mt-4 gap-3">
           <PlayButton movieId={movie.id} />
           <button
+            onClick={handleOpenModal}
             className="
               bg-white
               text-white
@@ -67,7 +148,8 @@ const Billboard: React.FC<BillboardProps> = ({ movie }) => {
               items-center
               hover:bg-opacity-20
               transition
-            ">
+            "
+          >
             <AiOutlineInfoCircle className="mr-1" />
             More Info
           </button>
@@ -75,6 +157,6 @@ const Billboard: React.FC<BillboardProps> = ({ movie }) => {
       </div>
     </div>
   );
-}
+};
 
 export default Billboard;
